fd65c4d73f336cf7a15cc88b582bfb73e646876b
Mayur Bahuguna <mayurbhgn108@gmail.com>
2021-03-26 15:01:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecommerce Project</h1>
+      <button>Normal Button</button>
+      <Button>Normal Button</Button>
+    </div>
+  );
+}
+
+export default App;
